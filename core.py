cd7d729f4c48af8bd317cd85333f5b4b8252ebae
Marko Grdinić <mrakgr@gmail.com>
2024-04-26 09:58:25 +0000

    Let me take a break here.

--- core.py
+++ core.py
@@ -2,3 +2,8 @@ from flask import Flask
 
 app = Flask('app', static_url_path="")
 app.config['SECRET_KEY'] = 'secret!'
+
+import supabase
+
+# https://supabase.com/docs/guides/cli/getting-started
+# https://supabase.com/docs/guides/database/full-text-search
