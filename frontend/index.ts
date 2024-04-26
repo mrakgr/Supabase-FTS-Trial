2f33b2ebf638aaff76786f090c5b5cd97b4b8a41
Marko Grdinić <mrakgr@gmail.com>
2024-04-26 09:53:00 +0000

    Setup the frontend.

--- frontend/index.ts
+++ frontend/index.ts
@@ -1,2 +1,9 @@
-const t = true;
+import {LitElement, css, html} from 'lit';
+import {customElement, property} from 'lit/decorators.js';
 
+@customElement('ui-main')
+class UI_Main extends LitElement {
+  render() {
+    return html`Hello.`;
+  }
+}
